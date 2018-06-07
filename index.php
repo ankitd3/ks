9d5fddaab93c866afe595912913113258bdee8da
ankitd3 <ankit.dalal488@gmail.com>
2018-06-07 12:35:31 +0530

    db check

--- index.php
+++ index.php
@@ -1,5 +1,28 @@
 <?php 
 
+$servername = "ip-172-31-28-80.ap-south-1.compute.internal";
+$username = "root";
+$password = "Namita&811";
+$dbname = "ks";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM expert";
+
+$result = $conn->query($sql);
+
+  if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+          $name1= $row["eid"];
+          echo $name1;
+      }
+  }
+
 $name = $_POST['name'];
 
 echo "In php";
