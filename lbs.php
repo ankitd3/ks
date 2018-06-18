2d97d97f2521f97a2f36d0865631041827ff39fe
ankitd3 <ankit.dalal488@gmail.com>
2018-06-18 14:17:31 +0530

    saving lanenumber to text file

--- lbs.php
+++ lbs.php
@@ -1,15 +1,10 @@
 <?php 
-echo "from server";
 
-$testOne = $_POST['one'];
-$testTwo = $_POST['two'];
-
+$l = htmlspecialchars($_GET['lane_number']);
+$fileName = "drive.txt";
+$myfile = fopen($fileName, "w") or die("Unable to open file!");
+fwrite($myfile, $l);
+fclose($myfile);
 #$lane_number = $_POST['l'];
-
 #echo $lane_number;
-
-echo $testOne;
-
-echo $testTwo;
-
- ?>
+?>
